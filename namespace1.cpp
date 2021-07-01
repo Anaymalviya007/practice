7b0b6d69a5805b024b1b9f6549ebd920aac642f5
DECODE <58022323+Anaymalviya007@users.noreply.github.com>
2021-07-02 00:03:14 +0530

    Add files via upload

---
+++ namespace1.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+namespace ns
+{
+
+	class geek
+	{
+	public:
+		void display()
+		{
+			cout << "ns::geek::display()\n";
+		}
+	};
+}
+
+int main()
+{
+
+	ns::geek obj;
+
+	obj.display();
+
+	return 0;
+}
